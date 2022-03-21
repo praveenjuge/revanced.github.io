f09dadc06cf512ddbe5877fb4487e450e3a231fe
she11sh0cked <22623152+she11sh0cked@users.noreply.github.com>
2022-03-21 23:05:31 +0100

    feat: add navbar

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,18 @@
 <script lang="ts">
+  import Tab, { Label } from "@smui/tab";
+  import TabBar from "@smui/tab-bar";
   import "../app.css";
 </script>
+
+<nav class="flex justify-between items-center p-5">
+  <h1 class="text-4xl font-bold">[ReVanced]</h1>
+  <div class="px-10 w-min rounded-lg mdc-elevation--z5">
+    <TabBar tabs={["Home", "Download", "Documentation"]} let:tab>
+      <Tab {tab}>
+        <Label>{tab}</Label>
+      </Tab>
+    </TabBar>
+  </div>
+</nav>
 
 <slot />

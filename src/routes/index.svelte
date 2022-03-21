d9df77171e08d63e3758706cf5ad869db97c55a8
she11sh0cked <22623152+she11sh0cked@users.noreply.github.com>
2022-03-22 00:05:35 +0100

    feat: add landing page hero

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,40 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<script>
+  import Button, { Icon, Label } from "@smui/button";
+</script>
+
+<div class="flex flex-col justify-center items-center space-y-10">
+  <h1 class="text-8xl font-bold">[RE]</h1>
+  <p class="text-xl font-semibold">
+    A simple, lightweight, and extensible framework for building ******* mods.
+  </p>
+  <div class="flex justify-center space-x-5">
+    <Button
+      color="primary"
+      variant="raised"
+      href="/download"
+      class="h-16 text-xl"
+    >
+      <Icon
+        class="material-icons"
+        style="font-size: 2rem; width: 2rem; height: 2rem; margin-right: 0.5rem;"
+      >
+        download
+      </Icon>
+      <Label>Download</Label>
+    </Button>
+    <Button
+      color="secondary"
+      variant="raised"
+      href="/docs"
+      class="h-16 text-xl"
+    >
+      <Icon
+        class="material-icons"
+        style="font-size: 2rem; width: 2rem; height: 2rem; margin-right: 0.5rem;"
+      >
+        book
+      </Icon>
+      <Label>Learn More</Label>
+    </Button>
+  </div>
+</div>

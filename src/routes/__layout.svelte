4ea4419b68077fa20fb2e6b5c0a01401ca9bb1a1
she11sh0cked <22623152+she11sh0cked@users.noreply.github.com>
2022-03-24 22:13:42 +0100

    fix: correct navigation reactivity

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -23,8 +23,7 @@
     },
   ];
 
-  let active = tabs.find((tab) => tab.path === $page.url.pathname);
-
+  $: active = tabs.find((tab) => tab.path === $page.url.pathname);
   $: withWaves = $page.url.pathname === tabs[0].path;
 </script>
 
@@ -38,7 +37,7 @@
   <nav class="flex justify-between items-center p-4">
     <h1 class="text-4xl font-bold">[ReVanced]</h1>
     <div class="px-10 w-min rounded-lg mdc-elevation--z5">
-      <TabBar {tabs} let:tab bind:active>
+      <TabBar {tabs} {active} let:tab>
         <Tab {tab} href={tab.path}>
           <Icon class="material-icons">{tab.icon}</Icon>
           <Label>{tab.label}</Label>

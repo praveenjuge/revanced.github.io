7007f2b363813c48ea404338c52fbeca25231c5f
she11sh0cked <22623152+she11sh0cked@users.noreply.github.com>
2022-03-24 21:00:59 +0100

    chore: add custom colors to tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,13 @@ const config = {
   content: ["./src/**/*.{html,js,svelte,ts}"],
 
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#ff4151",
+        secondary: "#282a36",
+        background: "#0f111a",
+      },
+    },
   },
 
   plugins: [],
